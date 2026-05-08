0fe54d0085ff4e9ab613620442c1df0ed3f13392
Anilkumar-Peepul <anil.pallekadi@peepulagri.com>
2026-05-08 18:33:27 +0530

    Update start_wsbrd.sh

--- scripts/start_wsbrd.sh
+++ scripts/start_wsbrd.sh
@@ -8,8 +8,17 @@ echo "[INFO] Generating wsbrd.conf..."
 
 python3 $BASE_DIR/scripts/generate_wsbrd_config.py
 
+UART_PORT=$(python3 -c "
+import json
+with open('$BASE_DIR/config/gateway.json') as f:
+    print(json.load(f)['uart_port'])
+")
+
+echo "[INFO] UART PORT: $UART_PORT"
+
 echo "[INFO] Starting wsbrd..."
 
-wsbrd \
+sudo wsbrd \
 -F $BASE_DIR/config/wsbrd.conf \
+-u $UART_PORT \
 -D
