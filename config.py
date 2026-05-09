b21dcd6bc48b056ebe21a8efd28632eacf4129af
Anilkumar-Peepul <anil.pallekadi@peepulagri.com>
2026-05-09 17:13:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import json
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 LOG_DIR = BASE_DIR / "logs"
 DATA_DIR = BASE_DIR / "data"
